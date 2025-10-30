9ea7dc7d0382858cff05cda1863f9c5407585736
Thomas Boigner <thomasboigner@outlook.com>
2025-10-30 20:23:17 +0100

    Add OcrWorker module

---
+++ OcrWorker/src/test/java/at/fhtw/ocrworker/OcrWorkerApplicationTest.java
@@ -0,0 +1,11 @@
+package at.fhtw.ocrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OcrWorkerApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
