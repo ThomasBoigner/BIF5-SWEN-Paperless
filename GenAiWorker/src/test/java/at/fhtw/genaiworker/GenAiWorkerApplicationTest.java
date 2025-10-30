6525d4ebf2e13f9cc93dde54d8b546419c496fd8
Thomas Boigner <thomasboigner@outlook.com>
2025-10-30 20:34:16 +0100

    Setup GenAiWorker project

---
+++ GenAiWorker/src/test/java/at/fhtw/genaiworker/GenAiWorkerApplicationTest.java
@@ -0,0 +1,11 @@
+package at.fhtw.genaiworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GenAiWorkerApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
