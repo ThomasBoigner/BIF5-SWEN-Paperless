c02a5218eaffc533ca30ce90287ddb5fa46bbb29
Thomas Boigner <thomasboigner@outlook.com>
2025-11-27 22:05:37 +0100

    Fix keycloak config

--- PaperlessRest/src/main/java/at/fhtw/paperlessrest/infrastructure/configuration/RabbitMQConfiguration.java
+++ PaperlessRest/src/main/java/at/fhtw/paperlessrest/infrastructure/configuration/RabbitMQConfiguration.java
@@ -3,7 +3,7 @@ package at.fhtw.paperlessrest.infrastructure.configuration;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfiguration {
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
+        return new JacksonJsonMessageConverter();
     }
 
     @Bean
