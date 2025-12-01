4bd57122b7f0711d2293c36e7fe72426ac840a50
Thomas Boigner <thomasboigner@outlook.com>
2025-12-01 23:05:53 +0100

    Add authentication to the backend

---
+++ PaperlessRest/src/main/java/at/fhtw/paperlessrest/infrastructure/configuration/SpringSecurityConfiguration.java
@@ -0,0 +1,24 @@
+package at.fhtw.paperlessrest.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.OPTIONS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+}
