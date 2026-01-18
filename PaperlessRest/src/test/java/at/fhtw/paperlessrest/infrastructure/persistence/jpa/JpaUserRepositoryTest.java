0a8dc55a07814bdcc7e73d5183cf555a85644193
Thomas Boigner <thomasboigner@outlook.com>
2026-01-18 22:29:44 +0100

    Fix tests

--- PaperlessRest/src/test/java/at/fhtw/paperlessrest/infrastructure/persistence/jpa/JpaUserRepositoryTest.java
+++ PaperlessRest/src/test/java/at/fhtw/paperlessrest/infrastructure/persistence/jpa/JpaUserRepositoryTest.java
@@ -3,10 +3,12 @@ package at.fhtw.paperlessrest.infrastructure.persistence.jpa;
 import at.fhtw.paperlessrest.domain.model.User;
 import at.fhtw.paperlessrest.domain.model.UserRepository;
 import at.fhtw.paperlessrest.domain.model.UserToken;
+import at.fhtw.paperlessrest.infrastructure.persistence.elasticsearch.FileDocumentRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -20,6 +22,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JpaUserRepositoryTest {
     @Autowired
     private UserRepository userRepository;
+
+    @MockitoBean
+    private FileDocumentRepository fileDocumentRepository;
 
     @Test
     public void ensureSaveWorksProperly() {
